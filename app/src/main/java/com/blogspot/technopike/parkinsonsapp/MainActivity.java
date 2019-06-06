9b1a443ffefedd2776c0a273db8b2a1a6f8a18f3
Jared <jaredmbannan@gmail.com>
2019-06-06 13:43:36 -0400

    Initial again

---
+++ app/src/main/java/com/blogspot/technopike/parkinsonsapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.blogspot.technopike.parkinsonsapp;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
