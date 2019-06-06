9f2a509d2340be4f277bc7f3d12bd9df8c60b74c
JBannan <jaredmbannan@gmail.com>
2019-06-06 14:55:11 -0400

    Prototype Accelerometer Data logged and displayed on screen

--- app/src/main/java/com/blogspot/technopike/parkinsonsapp/MainActivity.java
+++ app/src/main/java/com/blogspot/technopike/parkinsonsapp/MainActivity.java
@@ -1,19 +1,56 @@
 package com.blogspot.technopike.parkinsonsapp;
 
+import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final String TAG = "MainActivity";
     
     private SensorManager sensorManager;
     Sensor accelerometer;
+
+    TextView xValue, yValue, zValue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        xValue = (TextView) findViewById(R.id.xValue);
+        yValue = (TextView) findViewById(R.id.yValue);
+        zValue = (TextView) findViewById(R.id.zValue);
+
+        Log.d(TAG, "onCreate: Initializing Sensor Service");
+        
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        Log.d(TAG, "onCreate: Registered Accelerometer Listener");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Log.d(TAG, "onSensorChanged: X: " + sensorEvent.values[0] + " Y: " + sensorEvent.values[1] + " Z: " + sensorEvent.values[2]);
+
+        xValue.setText("xValue: " + sensorEvent.values[0]);
+        yValue.setText("yValue: " + sensorEvent.values[1]);
+        zValue.setText("zValue: " + sensorEvent.values[2]);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
