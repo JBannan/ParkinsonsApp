165c0168e8d330e7a5e90dc40307771c83a9753f
JBannan <jaredmbannan@gmail.com>
2019-07-13 17:13:38 -0400

    Added Tap class and layout for the tap test. Made the main activity more of a menu.

---
+++ app/src/main/java/com/blogspot/technopike/parkinsonsapp/TapHelper.java
@@ -0,0 +1,19 @@
+package com.blogspot.technopike.parkinsonsapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class TapHelper extends AppCompatActivity {
+
+    double previousClickTime;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
